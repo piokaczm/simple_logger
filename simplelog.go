36656d9aec6d0fb25eec116f84c765c0257ca30d
Piotr <piotr@positionly.com>
2017-01-05 21:00:21 +0100

    remove unused color var

--- simplelog.go
+++ simplelog.go
@@ -18,7 +18,6 @@ const (
 var (
 	red    = string([]byte{27, 91, 57, 55, 59, 52, 49, 109})
 	yellow = string([]byte{27, 91, 57, 55, 59, 52, 51, 109})
-	white  = string([]byte{27, 91, 57, 48, 59, 52, 55, 109})
 	green  = string([]byte{27, 91, 57, 55, 59, 52, 50, 109})
 	reset  = string([]byte{27, 91, 48, 109})
 )
